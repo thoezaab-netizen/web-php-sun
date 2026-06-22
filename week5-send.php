a034d510cc0da1d521ffc29e00f5655292a7fc42
thoezaab-netizen <thoezaab@gmail.com>
2026-06-22 16:18:25 +0700

    Create week5-send.php

---
+++ week5-send.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="th">
+
+<head>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="week5.receive.php" method="get">
+        <label>User name</label>
+        <input type="text" name="Username">
+        <br>
+
+        <label>Password</label>
+        <input type="password" name="UserPass">
+        <br>
+        <label>Email</label>
+        <input type="email" name="UserEmail">
+        <br>
+        <label>Age</label>
+        <input type="number" name="UserAge">
+        <br>
+        <input type="date" name="birthdate">
+        <br>
+        <textarea name="UserMsg"></textarea>
+        <br>
+        <input type="radio" name="gender" value="male"> ชาย
+        <input type="radio" name="gender" value="female"> หญิง
+        <br>
+        <label>จังหวัด</label>
+        <select name="Usercity">
+            <option value="กรุงเทพ">กรุงเทพ</option>
+            <option value="ไม่ระบุ">-</option>
+        </select>
+        <br>
+        <label>งานอดิเรก</label>
+        <input type="checkbox" name="hobby[]" value="football"> ฟุตบอล
+        <input type="checkbox" name="hobby[]" value="basketball"> บาสเกตบอล
+        <input type="checkbox" name="hobby[]" value="tennis"> เทนนิส
+        <br>
+        <button type="submit">ส่งข้อมูล</button>
+   </form>
+</body>
+
+</html>
